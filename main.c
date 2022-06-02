8f78178b021797e376c1e0497f8149e9c4a0774f
Vladimir Savin <vladimir@zerc.cc>
2022-06-02 16:25:57 +0100

    Quick MVP of the HTTP server.

--- main.c
+++ main.c
@@ -4,7 +4,98 @@
 #include <string.h>
 #include <libc.h>
 
+// Web server MVP
 int main() {
+    struct addrinfo hints, *res;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo("127.0.0.1", "8888", &hints, &res) != 0) {
+        perror("getaddrinfo error");
+        return 2;
+    }
+    printf("info collected\n");
+
+    int s;
+    if ((s = socket(PF_INET, res->ai_socktype, res->ai_protocol)) == -1) {
+        perror("socket error");
+        return 2;
+    }
+
+    int yes = 1;
+    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof yes) == -1) {
+        perror("setsockopt");
+        return 2;
+    }
+    printf("socket created\n");
+
+    if (bind(s, res->ai_addr, res->ai_addrlen) == -1) {
+        perror("bind error");
+        close(s);
+        return 2;
+    }
+
+    printf("socket binded\n");
+
+    if (listen(s, 10) == -1) {
+        perror("listen error");
+        close(s);
+        return 2;
+    }
+
+    printf("socket is listening\n");
+
+    int s_new;
+    struct sockaddr_storage inc_addr;
+    socklen_t inc_add_size;
+    inc_add_size = sizeof inc_addr;
+
+    char buff[1000];
+    int bytes_read;
+
+    printf("waiting for new connections\n");
+
+    while(1) {
+        if ((s_new = accept(s, (struct sockaddr *)&inc_addr, &inc_add_size)) == -1) {
+            perror("accept error");
+            close(s);
+            return 2;
+        }
+
+        if ((bytes_read = recv(s_new, buff, 999, 0)) == -1) {
+            perror('recv error');
+            close(s_new);
+            break;
+        }
+
+        buff[bytes_read] = '\0';
+        printf("received '%s'\n", buff);
+
+        char *msg = "HTTP/1.1 200 OK\nContent-Type: text/html;charset=utf-8\n\nHello!\n";
+        int len, bytes_sent;
+        len = strlen(msg);
+
+        if ((bytes_sent = send(s_new, msg, len, 0)) == -1) {
+            perror("send error");
+            close(s_new);
+            close(s);
+            return 2;
+        }
+
+        close(s_new);
+        break;
+    }
+
+    close(s);
+    printf("all done");
+    return 0;
+}
+
+// Magic packet MVP
+int main1() {
     struct addrinfo hints, *res;
 
     memset(&hints, 0, sizeof hints);
