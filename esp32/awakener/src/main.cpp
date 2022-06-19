eec4e365066aad96bbfe7387ee7f8b069740bdfb
Vladimir Savin <vladimir@zerc.cc>
2022-06-19 22:55:06 +0100

    Connect to WiFi.

--- esp32/awakener/src/main.cpp
+++ esp32/awakener/src/main.cpp
@@ -1,15 +1,36 @@
 #include <Arduino.h>
 #include <WiFi.h>
 
+void connectToWiFi(const char *ssid, const char *pwd);
+
 void setup()
 {
     Serial.begin(9600);
+    connectToWiFi(WIFI_NAME, WIFI_PASS);
 }
 
 void loop()
 {
     Serial.println("Hello World");
-    Serial.println(WIFI_NAME);
-    Serial.println(WIFI_PASS);
     delay(500);
+}
+
+void connectToWiFi(const char *ssid, const char *pwd)
+{
+    int ledState = 0;
+
+    Serial.println("Connecting to WiFi network: " + String(ssid));
+
+    WiFi.begin(ssid, pwd);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println();
+    Serial.println("WiFi connected!");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
